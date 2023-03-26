5cef1d294b2f5d70c01af9e65870b17145f554c7
DATWY <121526575+DATWY@users.noreply.github.com>
2023-03-26 23:28:11 +0700

    Update login_neon.js

--- login_neon.js
+++ login_neon.js
@@ -3,48 +3,51 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy0rWuiomfGmkvbYusXQQ0Exc8k1_myjs",
-  authDomain: "chat-on1.firebaseapp.com",
-  projectId: "chat-on1",
-  storageBucket: "chat-on1.appspot.com",
-  messagingSenderId: "30955784641",
-  appId: "1:30955784641:web:290bef4ff3dd70498a8740",
-  measurementId: "G-6TWCT3CL3G"
+  apiKey: "AIzaSyCeKmAb7bAwPhTXNAPFvK6Hpvl3QQsql2U",
+  authDomain: "chat-48060.firebaseapp.com",
+  databaseURL: "https://chat-48060-default-rtdb.firebaseio.com",
+  projectId: "chat-48060",
+  storageBucket: "chat-48060.appspot.com",
+  messagingSenderId: "1087245062669",
+  appId: "1:1087245062669:web:e4ccc21bd4361490b0bb44",
+  measurementId: "G-S477EP6FGT"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+window.onload = () => {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
 
-const messagesRef = collection(db, "messages");
+  const messagesRef = collection(db, "messages");
 
-document.getElementById("send").addEventListener("click", async () => {
-  const messageText = document.getElementById("message").value;
+  document.getElementById("send").addEventListener("click", async () => {
+    const messageText = document.getElementById("message").value;
 
-  if (messageText.trim() !== "") {
-    try {
-      await addDoc(messagesRef, {
-        text: messageText,
-        timestamp: new Date().toISOString()
-      });
-      document.getElementById("message").value = "";
-    } catch (e) {
-      console.error("Error adding document: ", e);
+    if (messageText.trim() !== "") {
+      try {
+        await addDoc(messagesRef, {
+          text: messageText,
+          timestamp: new Date().toISOString()
+        });
+        document.getElementById("message").value = "";
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
     }
-  }
-});
-
-const messagesQuery = query(messagesRef, orderBy("timestamp", "asc"));
-
-onSnapshot(messagesQuery, (querySnapshot) => {
-  const messagesContainer = document.getElementById("chat-messages");
-  messagesContainer.innerHTML = "";
-
-  querySnapshot.forEach((doc) => {
-    const messageElement = document.createElement("p");
-    messageElement.textContent = doc.data().text;
-    messagesContainer.appendChild(messageElement);
   });
 
-  messagesContainer.scrollTop = messagesContainer.scrollHeight;
-});
+  const messagesQuery = query(messagesRef, orderBy("timestamp", "asc"));
+
+  onSnapshot(messagesQuery, (querySnapshot) => {
+    const messagesContainer = document.getElementById("chat-messages");
+    messagesContainer.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+      const messageElement = document.createElement("p");
+      messageElement.textContent = doc.data().text;
+      messagesContainer.appendChild(messageElement);
+    });
+
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  });
+};
