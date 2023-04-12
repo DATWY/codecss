6e9b36bea981d91d6fb23c57c12bbbfe9d87b4bd
DATWY <121526575+DATWY@users.noreply.github.com>
2023-04-12 21:12:39 +0700

    Update Js_login_v1.js

--- Js_login_v1.js
+++ Js_login_v1.js
@@ -1,7 +1,7 @@
 
 
 if (localStorage.getItem('isLoggedIn') !== 'true') {
-    if (window.location.pathname !== '/login' && window.location.pathname !== '/p/login') {
+    if (window.location.pathname !== '/login' && window.location.pathname !== '/p/login.html') {
     window.location.href = "/login";
   } 
       }
