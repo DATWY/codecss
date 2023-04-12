953d6f8e9a255f51f3d5e77826981e9a5b24ad54
DATWY <121526575+DATWY@users.noreply.github.com>
2023-04-12 20:23:47 +0700

    Update Js_login_v1.js

--- Js_login_v1.js
+++ Js_login_v1.js
@@ -1,9 +1,6 @@
 // Kiểm tra nếu đang ở trang khác "/p/login.html" hoặc "/login"
 if(window.location.pathname !== "/p/login.html" && window.location.pathname !== "/login" ) {
-  // Tạo thẻ script
-  var script = document.createElement('script');
-  // Thiết lập src cho script
-  script.src = 'https://raw.githack.com/DATWY/codecss/main/Js_login_v2.js';
-  // Chèn thẻ script vào phần head của trang
-  document.head.appendChild(script);
+  if (localStorage.getItem('isLoggedIn') === 'falue') {
+      // Đăng nhập tự động nếu thông tin đăng nhập còn hiệu lực
+      window.location.href = "/login"; // Chuyển hướng đến trang login
 }
