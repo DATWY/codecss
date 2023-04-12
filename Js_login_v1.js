4418fa82d80fb3eae52ecf48cb80fa2e204481b2
DATWY <121526575+DATWY@users.noreply.github.com>
2023-04-12 20:25:08 +0700

    Update Js_login_v1.js

--- Js_login_v1.js
+++ Js_login_v1.js
@@ -1,6 +1,6 @@
 // Kiểm tra nếu đang ở trang khác "/p/login.html" hoặc "/login"
 if(window.location.pathname !== "/p/login.html" && window.location.pathname !== "/login" ) {
-  if (localStorage.getItem('isLoggedIn') === 'falue') {
+  if (localStorage.getItem('isLoggedIn') === 'false') {
       // Đăng nhập tự động nếu thông tin đăng nhập còn hiệu lực
       window.location.href = "/login"; // Chuyển hướng đến trang login
 }
