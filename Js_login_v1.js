e8c97db84eb3cc272b9738bed7f0dfa109a6d843
DATWY <121526575+DATWY@users.noreply.github.com>
2023-04-13 19:06:51 +0700

    Update Js_login_v1.js

--- Js_login_v1.js
+++ Js_login_v1.js
@@ -1,7 +1,21 @@
 
+// Hàm kiểm tra trạng thái đăng nhập
+function checkLoginStatus() {
+  if (localStorage.getItem('isLoggedIn') === 'true') {
+    var expirationDate = new Date(localStorage.getItem('expirationDate'));
+    if (expirationDate < new Date()) {
+      localStorage.removeItem('isLoggedIn');
+      localStorage.removeItem('username');
+      localStorage.removeItem('expirationDate');
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
 
-if (localStorage.getItem('isLoggedIn') !== 'true') {
-    if (window.location.pathname !== '/login') {
-    window.location.href = "/login";
-  } 
-      }
+// Kiểm tra trạng thái đăng nhập và chuyển hướng nếu cần
+if (!checkLoginStatus()) {
+  window.location.href = "/login";
+}
